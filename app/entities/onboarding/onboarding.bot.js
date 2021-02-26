dd5cd1cc0314114511a7a7eb8b7cf4c9d1d2e045
Thanos Polychronakis <thanpolas@gmail.com>
2021-02-26 11:05:08 +0200

    add examples of DMing on discord

--- app/entities/onboarding/onboarding.bot.js
+++ app/entities/onboarding/onboarding.bot.js
@@ -13,12 +13,18 @@ onboarding.init = () => {
   const client = discordService.getClient();
 
   // Create an event listener for new guild members
-  client.on('guildMemberAdd', (member) => {
+  client.on('guildMemberAdd', async (member) => {
     console.log('ADD:', member);
+    if (!member) {
+      return;
+    }
+
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.cache.find(
-      (ch) => ch.name === 'bot-test',
-    );
+    // const channel = member.guild.channels.cache.find(
+    //   (ch) => ch.name === 'bot-test',
+    // );
+
+    const channel = await member.createDM();
 
     // Do nothing if the channel wasn't found on this server
     if (!channel) {
@@ -26,6 +32,18 @@ onboarding.init = () => {
     }
 
     // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}`);
+    await channel.send(`Welcome to the server, ${member}`);
+  });
+
+  client.on('message', (message) => {
+    if (message.content === 'ayy') {
+      message.channel.send('Ayy, lmao!');
+    }
+    if (message.content === 'wat') {
+      message.channel.send('Say what?');
+    }
+    if (message.content === 'lol') {
+      message.channel.send('roflmaotntpmp');
+    }
   });
 };
