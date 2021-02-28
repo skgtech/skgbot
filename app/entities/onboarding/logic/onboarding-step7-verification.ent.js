03001812d72f3a6b6039188e0ce7eee496608bc8
Thanos Polychronakis <thanpolas@gmail.com>
2021-02-28 14:53:31 +0200

    work on step7 - email verification

--- app/entities/onboarding/logic/onboarding-step7-verification.ent.js
+++ app/entities/onboarding/logic/onboarding-step7-verification.ent.js
@@ -2,10 +2,12 @@
  * @fileoverview Step 7, email verification.
  */
 
-const { validate: uuidValidate } = require('uuid');
+const addDt = require('date-fns/add');
+const config = require('config');
+const { v4: uuid, validate: uuidValidate } = require('uuid');
 
 const { db } = require('../../../services/postgres.service');
-const emailService = require('../../../services/email.service');
+const step6 = require('./onboarding-step6-nickname.ent');
 const { step7Error, step7Success, step7ErrorNoMatch } = require('../messages');
 const { update } = require('../../members/members.ent');
 const log = require('../../../services/log.service').get();
@@ -51,6 +53,51 @@ step.handle7 = async (message, localMember) => {
 };
 
 /**
+ * Will resend the verification email, if needed it will reset the token
+ * and update the expiration date.
+ *
+ * @param {Member} localMember The local member record.
+ * @return {Promise<void>} A Promise.
+ */
+step.resendVerification = async (localMember) => {
+  log.info('Resend Verification requested.', localMember);
+
+  const nowDt = new Date();
+  const expireDt = new Date(localMember.verification_code_expires_at);
+
+  let verificationCode = localMember.verification_code;
+  if (nowDt > expireDt) {
+    // Verification Expired, issue a new one.
+    verificationCode = await step._resetVerification(localMember);
+  }
+
+  // Prepare and dispatch the verification email
+  await step6.sendVerificationEmail(localMember, verificationCode);
+};
+
+/**
+ * Resets the verification token and expiration date of the member.
+ *
+ * @param {Member} localMember The local member record.
+ * @return {Promise<string>} A promise with the new verification token.
+ * @private
+ */
+step._resetVerification = async (localMember) => {
+  const verification_code = uuid();
+  const nowDt = new Date();
+  const verification_code_expires_at = addDt(nowDt, {
+    days: config.onboarding.verification_expires_days,
+  });
+  await update(localMember.discord_uid, {
+    onboarding_state: 'email_verification',
+    verification_code,
+    verification_code_expires_at,
+  });
+
+  return verification_code;
+};
+
+/**
  * Enable the member to use the discord server, add roles and update record.
  *
  * @param {Member} localMember The local member record.
@@ -60,6 +107,26 @@ step.handle7 = async (message, localMember) => {
 step._enableMember = async (localMember) => {
   await step._enableMemberDiscord(localMember);
 
+  await step._enableMemberUpdateRecord(localMember);
+};
+
+/**
+ * Add discord roles to member.
+ *
+ * @param {Member} localMember The local member record.
+ * @return {Promise<void>} A Promise.
+ * @private
+ */
+step._enableMemberDiscord = async (localMember) => {};
+
+/**
+ * Update the record of the member.
+ *
+ * @param {Member} localMember The local member record.
+ * @return {Promise<void>} A Promise.
+ * @private
+ */
+step._enableMemberUpdateRecord = async (localMember) => {
   const updateData = {
     onboarding_state: 'member',
     is_onboarded: true,
@@ -67,5 +134,3 @@ step._enableMember = async (localMember) => {
   };
   await update(localMember.discord_uid, updateData);
 };
-
-step._enableMemberDiscord = async (localMember) => {};
