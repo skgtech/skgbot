6f0570688d1bc680ae3569d6d49db8bcb07ae775
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-01 14:31:01 +0200

    update member's help

--- app/entities/message-router/messages.js
+++ app/entities/message-router/messages.js
@@ -4,7 +4,23 @@
 
 const messages = (module.exports = {});
 
-messages.help = () => 'HELP!!!';
+messages.help = () => `Available commands to members:
+
+* \`!help\` :: This help screen.
+* \`!profile [username]\` Show the profile of another member.
+* \`!nickname [nickname]\` Update your nickname.
+* \`!bio [bio]\` Update your bio, multi-line inputs are allowed.
+* \`!join [Category]\` Join a topic category of channels (more bellow).
+* \`!part [Category]\` Leave a topic category of channels (more bellow).
+
+Available topic Categories in this server:
+
+* \`Engineer\` Engineering topics, programming languages, devops, etc.
+* \`Product\` Product, design, remote, business and leadership topics.
+* \`Hobbies\` Real-life hobbies, movies, music, space, IoT, gardening, etc.
+* \`Investor\` Channels related to investing and cryptos.
+* \`Politics\` Politics and Pandemic related channels.`;
+
 messages.helpOnboarding = () => 'HELP Onboarding!!!';
 messages.error = () =>
   'Unknown command, type `!help` for a list of available commands.';
