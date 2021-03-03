6a05d54928670891a3f128f8117197e4af84fb77
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-03 23:14:13 +0200

    decouple setNickname and fix nickname command

---
+++ app/entities/discord/logic/change-nickname.ent.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Changes the nickname of a discord member.
+ */
+
+const { getGuildMember } = require('./guild.ent');
+const log = require('../../../services/log.service').get();
+
+const entity = (module.exports = {});
+
+/**
+ * Set the member's nickname on discord.
+ *
+ * @param {DiscordMessage} message The discord message.
+ * @param {Member} localMember Local record of the member.
+ * @param {string} nickname The nickname to set the user with.
+ * @return {Promise<void>} A Promise.
+ */
+entity.setNickname = async (message, localMember, nickname) => {
+  const guildMember = await getGuildMember(message);
+  try {
+    await guildMember.setNickname(nickname);
+  } catch (ex) {
+    log.error('Failed to set nickname', {
+      localMember,
+      error: ex,
+      custom: { nickname },
+    });
+
+    // do not propagate error on purpose.
+  }
+};
