544efcbf046991406e1d223448bcef9066f540b6
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-04 20:11:37 +0200

    scaffold all moderation commands

--- app/entities/moderation/moderation.ent.js
+++ app/entities/moderation/moderation.ent.js
@@ -2,7 +2,14 @@
  * @fileoverview Moderation related entities.
  */
 
+const { moderationBan } = require('./logic/moderation-ban.ent');
+const { moderationUnban } = require('./logic/moderation-unban.ent');
+const { moderationList } = require('./logic/moderation-list.ent');
+const { moderationListAll } = require('./logic/moderation-list-all.ent');
+
 const entity = (module.exports = {});
 
 entity.moderationBan = moderationBan;
 entity.moderationUnban = moderationUnban;
+entity.moderationList = moderationList;
+entity.moderationListAll = moderationListAll;
