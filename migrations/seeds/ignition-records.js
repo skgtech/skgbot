bd35cf12d8584eaff61747f80a1e001594f61d5b
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-04 22:25:51 +0200

    create seed and tweak reset db scripts

--- migrations/seeds/ignition-records.js
+++ migrations/seeds/ignition-records.js
@@ -12,4 +12,30 @@ const seed = (module.exports = {});
  * @param {Object} knex Knex instance.
  * @return {Promise}
  */
-seed.seed = async (/* knex */) => {};
+seed.seed = async (knex) => {
+  const members = [
+    {
+      discord_uid: '291828960872890368',
+      email: 'thanpolas@gmail.com',
+      username: 'thanpolas',
+      nickname: 'thanpolas',
+      first_name: 'Thanasis',
+      last_name: 'Polychronakis',
+      is_onboarded: true,
+      onboarded_at: '2021-03-04 20:07:06.716969+00',
+      bio: 'the bio',
+      onboarding_state: 'member',
+      verification_code: '...',
+      verification_code_expires_at: '2021-03-11 20:06:13.931+00',
+      joined_at: '2021-02-17 07:25:59.533+00',
+      created_at: '2021-03-04 20:05:41.981843+00',
+      updated_at: '2021-03-04 20:07:06.716969+00',
+    },
+  ];
+  const promises = members.map((member) => {
+    console.log('inserting', member.discord_uid);
+    return knex.insert(member).into('members');
+  });
+
+  await Promise.all(promises);
+};
