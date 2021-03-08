371ad9495d54100371d82d049edce0b51b824cc7
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-08 14:38:06 +0200

    mock and test follow-up 1

--- test/app/follow-up-joined.test.js
+++ test/app/follow-up-joined.test.js
@@ -5,12 +5,20 @@
 const testLib = require('../lib/test.lib');
 const { createMulty, deleteMulti } = require('../setup/member.setup');
 const { followUpJoined1 } = require('../../app/entities/onboarding');
+const guildEnt = require('../../app/entities/discord/logic/guild.ent');
 
 describe('Follow-up Joined 1', () => {
   testLib.init();
 
   describe('Happy Path', () => {
     test('Will produce 3 follow up actions', async () => {
+      const sendMock = jest.fn(() => Promise.resolve());
+      guildEnt.getGuildMemberUid = jest.fn(() =>
+        Promise.resolve({
+          send: sendMock,
+        }),
+      );
+
       // create 3 new members and 2 full members.
       const allMembers = await createMulty([
         { memberType: 'new' },
@@ -22,6 +30,8 @@ describe('Follow-up Joined 1', () => {
 
       // execute the follow up 1 task.
       await followUpJoined1();
+
+      expect(sendMock.mock.calls.length).toBe(3);
 
       await deleteMulti(allMembers);
     });
