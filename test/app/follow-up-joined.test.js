82c3e7dd428386560fcf22fc142e7f57d6ccc561
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-10 16:53:51 +0200

    update references on follow-up-joined test

--- test/app/follow-up-joined.test.js
+++ test/app/follow-up-joined.test.js
@@ -6,9 +6,13 @@ const subDt = require('date-fns/sub');
 
 const testLib = require('../lib/test.lib');
 const { createMulty, deleteMulti } = require('../setup/member.setup');
-const { followUpJoined1 } = require('../../app/entities/onboarding');
+const {
+  followUpJoined1: followUpJoined1Task,
+} = require('../../app/entities/onboarding-followup');
 const guildEnt = require('../../app/entities/discord/logic/guild.ent');
-const { followupJoined1 } = require('../../app/entities/onboarding/messages');
+const {
+  followUpJoined1,
+} = require('../../app/entities/onboarding-followup/messages');
 const {
   getByMemberIds,
 } = require('../../app/entities/onboarding-followup/sql/onboard-track.sql');
@@ -55,31 +59,31 @@ describe('Follow-up Joined 1', () => {
     });
     test('Will produce 3 follow up actions', async () => {
       // execute the follow-up-1 task.
-      await followUpJoined1();
+      await followUpJoined1Task();
 
       expect(sendMock.mock.calls.length).toBe(3);
     });
     test('Will produce expected messages to members', async () => {
       // execute the follow-up-1 task.
-      await followUpJoined1();
+      await followUpJoined1Task();
 
       const { calls } = sendMock.mock;
 
       const allCallMessages = [calls[0][0], calls[1][0], calls[2][0]];
 
       expect(allCallMessages).toInclude(
-        followupJoined1(allMembers[0].username),
+        followUpJoined1(allMembers[0].username),
       );
       expect(allCallMessages).toInclude(
-        followupJoined1(allMembers[1].username),
+        followUpJoined1(allMembers[1].username),
       );
       expect(allCallMessages).toInclude(
-        followupJoined1(allMembers[2].username),
+        followUpJoined1(allMembers[2].username),
       );
     });
     test('Will produce expected records', async () => {
       // execute the follow-up-1 task.
-      await followUpJoined1();
+      await followUpJoined1Task();
 
       const memberIds = [
         allMembers[0].discord_uid,
