0c7e27ddf0b53f25c1719a688ee5bb9e760cdd36
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-11 17:27:57 +0200

    implement check missing functionality

--- app/entities/onboarding-followup/onboarding-followup.ent.js
+++ app/entities/onboarding-followup/onboarding-followup.ent.js
@@ -2,6 +2,7 @@
  * @fileoverview Onboarding of Discord users.
  */
 
+const { run: checkMissing } = require('./logic/check-missing.ent');
 const { run: followUpJoined1 } = require('./logic/followup-joined1.ent');
 const { run: followUpDaily } = require('./logic/followup-daily.ent');
 
@@ -10,3 +11,4 @@ const onboarding = (module.exports = {});
 // export the methods
 onboarding.followUpJoined1 = followUpJoined1;
 onboarding.followUpDaily = followUpDaily;
+onboarding.checkMissing = checkMissing;
