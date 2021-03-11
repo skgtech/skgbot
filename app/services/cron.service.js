a87104b90a914c70544a1ebcc08c0d7a3fd68bde
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-11 15:34:43 +0200

    log cron init and make async

--- app/services/cron.service.js
+++ app/services/cron.service.js
@@ -4,6 +4,8 @@
 
 const cron = require('node-cron');
 
+const log = require('./log.service').get();
+
 const {
   followUpJoined1,
   followUpDaily,
@@ -11,7 +13,13 @@ const {
 
 const service = (module.exports = {});
 
-service.init = () => {
+/**
+ * Intialize the task manager (Cron) service.
+ *
+ * @return {Promise<void>} A Promise.
+ */
+service.init = async () => {
+  await log.info('Initializing CRON Service...');
   // Run every 10 minutes.
   cron.schedule('*/10 * * * *', followUpJoined1);
 
