7832555cb171a8765c0a40f82ca9b8edb90e9ceb
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-15 19:16:41 +0200

    create getRandomInt helper function

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -49,3 +49,14 @@ helpers.splitString = (str, numChars = 1800) => {
 
   return ret;
 };
+
+/**
+ * Returns a random number from 0 up to a total of maximum numbers
+ * (not inclusive) as defined.
+ *
+ * @param {number} max Maximum random number to return.
+ * @return {number} A random integer number.
+ */
+helpers.getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
