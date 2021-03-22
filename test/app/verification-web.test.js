e01b26a38c134f6ab6f6b07be5bc8d450d34d145
Thanos Polychronakis <thanpolas@gmail.com>
2021-03-22 13:46:12 +0200

    test to ensure double verification click error

--- test/app/verification-web.test.js
+++ test/app/verification-web.test.js
@@ -16,38 +16,39 @@ const { getById: getMemberById } = require('../../app/entities/members');
 describe('Verification Web', () => {
   testLib.init();
 
-  describe('Happy Path', () => {
-    let memberFix;
-    let sendMock;
-    beforeEach(async () => {
-      sendMock = jest.fn(() => Promise.resolve());
-      const guildMock = () =>
-        Promise.resolve({
-          send: sendMock,
-        });
-      discordEnt.getGuildMemberLocal = jest.fn(guildMock);
-      discordEnt.getMainChannel = jest.fn(() => {
-        return {
-          send: sendMock,
-        };
+  let memberFix;
+  let sendMock;
+  beforeEach(async () => {
+    sendMock = jest.fn(() => Promise.resolve());
+    const guildMock = () =>
+      Promise.resolve({
+        send: sendMock,
       });
-      discordEnt.applyRolesToNewMember = jest.fn(() => Promise.resolve());
+    discordEnt.getGuildMemberLocal = jest.fn(guildMock);
+    discordEnt.getMainChannel = jest.fn(() => {
+      return {
+        send: sendMock,
+      };
+    });
+    discordEnt.applyRolesToNewMember = jest.fn(() => Promise.resolve());
 
-      // create members.
-      const nowDt = new Date();
-      const dtFrom = subDt(nowDt, {
-        minutes: 10,
-      });
-      memberFix = await createMember({
-        memberType: 'new',
-        joinedAt: dtFrom,
-        isOnboarded: false,
-        onboardingState: 'email_verification',
-      });
+    // create members.
+    const nowDt = new Date();
+    const dtFrom = subDt(nowDt, {
+      minutes: 10,
     });
-    afterEach(async () => {
-      await deleteMember(memberFix);
+    memberFix = await createMember({
+      memberType: 'new',
+      joinedAt: dtFrom,
+      isOnboarded: false,
+      onboardingState: 'email_verification',
     });
+  });
+  afterEach(async () => {
+    await deleteMember(memberFix);
+  });
+
+  describe('Happy Path', () => {
     test('Will successfully verify via web and update record', async () => {
       const agent = testLib.getAgent();
       const res = await agent.get(`/verify/${memberFix.verification_code}`);
@@ -67,6 +68,22 @@ describe('Verification Web', () => {
 
       const { calls } = sendMock.mock;
       expect(calls.length).toBe(2);
+    });
+  });
+  describe('Error Cases', () => {
+    test('Should not allow double invocation of verificaiton operation', async () => {
+      const agent = testLib.getAgent();
+      const res = await agent.get(`/verify/${memberFix.verification_code}`);
+
+      expect(res.statusCode).toEqual(200);
+
+      const resTwo = await agent.get(`/verify/${memberFix.verification_code}`);
+
+      expect(resTwo.statusCode).toEqual(200);
+      expect(resTwo.text).toInclude('Operation Failed');
+      const { calls } = sendMock.mock;
+      expect(calls.length).toBe(3);
+      expect(calls[2][0]).toEqual('You are not allowed to join this server.');
     });
   });
 });
